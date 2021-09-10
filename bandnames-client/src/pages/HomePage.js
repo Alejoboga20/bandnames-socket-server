144136352c8de02293ee814ec4a49a3d4f25494c
Alejo Boga <alejoboga20@gmail.com>
2021-09-10 16:04:11 -0300

    Adding real time graph

--- bandnames-client/src/pages/HomePage.js
+++ bandnames-client/src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { BandAdd } from '../components/BandAdd';
+import { BandChart } from '../components/BandChart';
 import { BandList } from '../components/BandList';
 import { SocketContext } from '../context/SocketContext';
 
@@ -20,6 +21,12 @@ const HomePage = () => {
 			</div>
 			<h1>BandNames</h1>
 			<hr />
+
+			<div className='row'>
+				<div className='col-8'>
+					<BandChart />
+				</div>
+			</div>
 
 			<div className='row'>
 				<div className='col-8'>
