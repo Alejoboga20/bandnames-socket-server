5138ce664913cac28490fa47db7316efb4689078
Alejo Boga <alejoboga20@gmail.com>
2021-09-10 09:57:38 -0300

    Creating first components

--- bandnames-client/src/App.js
+++ bandnames-client/src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { BandAdd } from './components/BandAdd';
+import { BandList } from './components/BandList';
 
 const App = () => {
-	return <div>Hello sockets</div>;
+	return (
+		<div className='container'>
+			<div className='aler'>
+				<p>
+					Service Status:
+					<span className='text-success'>Online</span>
+					<span className='text-danger'>Offline</span>
+				</p>
+			</div>
+			<h1>BandNames</h1>
+			<hr />
+
+			<div className='row'>
+				<div className='col-8'>
+					<BandList />
+				</div>
+				<div className='col-4'>
+					<BandAdd />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
---
+++ bandnames-client/src/components/BandAdd.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const BandAdd = () => {
+	return (
+		<>
+			<h3>Add Band</h3>
+
+			<form action=''>
+				<input
+					type='text'
+					className='form-control'
+					placeholder='new band name'
+				/>
+			</form>
+		</>
+	);
+};
