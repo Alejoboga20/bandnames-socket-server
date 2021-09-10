8672e61cc05d76e9a5d23f61d5d06d94bbb9ffb4
Alejo Boga <alejoboga20@gmail.com>
2021-09-10 10:33:04 -0300

    Editing band names

--- bandnames-client/src/components/BandList.js
+++ bandnames-client/src/components/BandList.js
@@ -7,6 +7,23 @@ export const BandList = ({ data }) => {
 		setBands(data);
 	}, [data]);
 
+	const onChangeName = (event, id) => {
+		const newName = event.target.value;
+
+		setBands((bands) =>
+			bands.map((band) => {
+				if (band.id === id) {
+					band.name = newName;
+				}
+				return band;
+			})
+		);
+	};
+
+	const onLostFocus = (id, name) => {
+		console.log(id, name);
+	};
+
 	const createRows = () => {
 		return bands.map((band) => (
 			<tr key={band.id}>
@@ -14,10 +31,16 @@ export const BandList = ({ data }) => {
 					<button className='btn btn-primary'>+1</button>
 				</td>
 				<td>
-					<input type='text' className='form-control' value={band.name} />
+					<input
+						type='text'
+						className='form-control'
+						value={band.name}
+						onChange={(event) => onChangeName(event, band.id)}
+						onBlur={() => onLostFocus(band.id, band.name)}
+					/>
 				</td>
 				<td>
-					<h3>15</h3>
+					<h3>{band.votes}</h3>
 				</td>
 				<td>
 					<button className='btn btn-danger'>Delete</button>
