8b9692584d24fca92c40d85c43d83002b8354a4a
Alejo Boga <alejoboga20@gmail.com>
2021-09-10 09:31:08 -0300

    Sending socket events

--- models/sockets.js
+++ models/sockets.js
@@ -1,15 +1,17 @@
+const BandList = require('./band-list');
+
 class Sockets {
 	constructor(io) {
 		this.io = io;
+		this.bandList = new BandList();
 		this.socketEvents();
 	}
 
 	socketEvents() {
 		this.io.on('connection', (socket) => {
-			socket.on('msg-to-server', (data) => {
-				console.log(data);
-				this.io.emit('msg-from-server', data);
-			});
+			console.log('client connected');
+
+			socket.emit('current-bands', this.bandList.getBands());
 		});
 	}
 }
